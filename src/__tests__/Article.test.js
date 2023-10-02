b29e58ad2a9f01404b1d36c3ad411eb1d454cd57
Dane Palazzo <PalazzoDD@comcast.net>
2023-10-02 11:18:10 -0600

    Completed Running Test Lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,22 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// src/__tests__/Article.test.js
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  // add this line
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
